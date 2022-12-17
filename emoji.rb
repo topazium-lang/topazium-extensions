d8844fd53bf48320e0393f540fd80c336c250de9
topazium-lang <120594607+topazium-lang@users.noreply.github.com>
2022-12-17 09:07:45 -0300

    emoji extension

---
+++ emoji.rb
@@ -0,0 +1,35 @@
+class Emoji
+    def self.happy
+        "😃"
+    end
+    def self.sad
+        "😔"
+    end
+    def self.cry
+        "😢"
+    end
+    def self.sunglasses
+        "😎"
+    end
+    def self.lol
+        "🤣"
+    end
+    def self.nerd
+        "🤓"
+    end
+    def self.party
+        "🥳"
+    end
+    def self.kiss
+        "😘"
+    end
+    def self.money
+        "🤑"
+    end
+    def self.hug
+        "🤗"
+    end
+    def self.sleep
+        "😴"
+    end
+end
